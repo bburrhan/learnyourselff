241e57ae14b9df098b78d773a220df9dc49230c9
bburrhan <burhan.isik@gmail.com>
2025-08-17 01:11:57 +0300

    Fix missing useEffect imports

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route, Link, useLocation } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { useAuth } from '../../hooks/useAuth'
