32b41f1941639022de52a2115055e472be5b895e
bburrhan <burhan.isik@gmail.com>
2026-02-25 13:32:56 +0300

    Updated index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Roboto', 'ui-sans-serif', 'system-ui', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+      },
       colors: {
         'royal-blue': {
           50: '#eff6ff',
